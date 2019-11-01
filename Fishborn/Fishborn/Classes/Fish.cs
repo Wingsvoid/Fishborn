1a99e06eb9c8f5759c8ea79b819c200f025b171c
51ch <elvendirian@gmail.com>
2019-11-01 22:11:01 +0700

    Go to Git

---
+++ Fishborn/Fishborn/Classes/Fish.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fishborn
+{
+    class Fish
+    {
+        private Point position;
+        private float speed;
+        private float visibility;
+        private float starvetime;
+        private float timelife;
+
+        public float Speed { get => speed; }
+        public float Visibility { get => visibility; }
+        public float Starve_time { get => starvetime;}
+        public float Timelife { get => timelife;}
+        public Fish(float _speed, float _visibility, float _starvetime, Point _pos)
+        {
+            position = _pos;
+            speed = _speed;
+            visibility = _visibility;
+            starvetime = _starvetime;
+            timelife = 0;
+        }
+        public void UpTimelife(float time)
+        {
+            timelife += time;
+        }
+    }
+}
---
+++ Fishborn/Fishborn/Classes/Simulation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fishborn
+{
+    class Simulation
+    {
+        private List<Fish> Fishes;
+        private List<Plant> Plants;
+    }
+}
